8179429d2849683ad84325a1a5f188e54959da2f
Dave Glassanos <david.glassanos@gmail.com>
2022-05-28 10:47:54 -0700

    Add default tables

---
+++ src/Tracket/Core/database/migrations/2022_05_04_035918_create_companies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('external_id');
+            $table->string('name');
+            $table->string('tagline');
+            $table->text('description');
+            $table->string('website_url');
+            $table->string('linkedin_url')->nullable();
+            $table->string('twitter_url')->nullable();
+            $table->string('blog_url')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
